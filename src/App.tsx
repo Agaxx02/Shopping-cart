ade5c36cb16308e8a71a570da73ef49972e77e91
agnieszka <agaxx02@gmail.com>
2022-09-27 19:33:05 +0200

    Added navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import './App.css';
+import './App.scss';
+import Navbar from './components/Navbar';
 
 function App() {
 	return (
 		<div className='App'>
-			<h1>E-commerence</h1>
+			<Navbar></Navbar>
 		</div>
 	);
 }
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+function Navbar() {
+	return (
+		<div className='Navbar'>
+			<div className='cartContainer'>
+				<img className='cartIcon' alt='shopping cart' src='./shopping-cart.png'></img>
+			</div>
+		</div>
+	);
+}
+
+export default Navbar;
